6529c4346061c1ccc51dfbb2b916a8fb1331f345
Chris Bryan <cbryan16@asu.edu>
2020-09-15 11:26:54 -0700

    Initial commit

---
+++ scripts/homework3.js
@@ -0,0 +1,117 @@
+
+var mapSvg;
+
+var lineSvg;
+var lineWidth;
+var lineHeight;
+var lineInnerHeight;
+var lineInnerWidth;
+var lineMargin = { top: 20, right: 60, bottom: 60, left: 100 };
+
+var mapData;
+var timeData;
+
+// This runs when the page is loaded
+document.addEventListener('DOMContentLoaded', function() {
+  mapSvg = d3.select('#map');
+  lineSvg = d3.select('#linechart');
+  lineWidth = +lineSvg.style('width').replace('px','');
+  lineHeight = +lineSvg.style('height').replace('px','');;
+  lineInnerWidth = lineWidth - lineMargin.left - lineMargin.right;
+  lineInnerHeight = lineHeight - lineMargin.top - lineMargin.bottom;
+
+  // Load both files before doing anything else
+  Promise.all([d3.json('data/africa.geojson'),
+               d3.csv('data/africa_gdp_per_capita.csv')])
+          .then(function(values){
+    
+    mapData = values[0];
+    timeData = values[1];
+   
+    drawMap();
+  })
+
+});
+
+// Get the min/max values for a year and return as an array
+// of size=2. You shouldn't need to update this function.
+function getExtentsForYear(yearData) {
+  var max = Number.MIN_VALUE;
+  var min = Number.MAX_VALUE;
+  for(var key in yearData) {
+    if(key == 'Year') 
+      continue;
+    let val = +yearData[key];
+    if(val > max)
+      max = val;
+    if(val < min)
+      min = val;
+  }
+  return [min,max];
+}
+
+// Draw the map in the #map svg
+function drawMap() {
+
+  // create the map projection and geoPath
+  let projection = d3.geoMercator()
+                      .scale(400)
+                      .center(d3.geoCentroid(mapData))
+                      .translate([+mapSvg.style('width').replace('px','')/2,
+                                  +mapSvg.style('height').replace('px','')/2.3]);
+  let path = d3.geoPath()
+               .projection(projection);
+
+  // get the selected year based on the input box's value
+  var year = "2000";
+
+  // get the GDP values for countries for the selected year
+  let yearData = timeData.filter( d => d.Year == year)[0];
+  
+  // get the min/max GDP values for the selected year
+  let extent = getExtentsForYear(yearData);
+
+  // get the selected color scale based on the dropdown value
+  var colorScale = d3.scaleSequential(d3.interpolateRdYlGn)
+                     .domain(extent);
+
+  
+  // draw the map on the #map svg
+  let g = mapSvg.append('g');
+  g.selectAll('path')
+    .data(mapData.features)
+    .enter()
+    .append('path')
+    .attr('d', path)
+    .attr('id', d => { return d.properties.name})
+    .attr('class','countrymap')
+    .style('fill', d => {
+      let val = +yearData[d.properties.name];
+      if(isNaN(val)) 
+        return 'white';
+      return colorScale(val);
+    })
+    .on('mouseover', function(d,i) {
+      console.log('mouseover on ' + d.properties.name);
+    })
+    .on('mousemove',function(d,i) {
+      console.log('mousemove on ' + d.properties.name);
+    })
+    .on('mouseout', function(d,i) {
+      console.log('mouseout on ' + d.properties.name);
+    })
+    .on('click', function(d,i) {
+      console.log('clicked on ' + d.properties.name);
+    });
+    
+}
+
+
+// Draw the line chart in the #linechart svg for
+// the country argument (e.g., `Algeria').
+function drawLineChart(country) {
+
+  if(!country)
+    return;
+  
+}
